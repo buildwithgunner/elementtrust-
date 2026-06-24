a552b4152f66d03e3ac616ca0e93a386f8bd24cb
Almightymayor <danielmoses849@gmail.com>
2026-06-24 09:42:49 +0200

    fix: update allowed cors origins for vercel

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['https://elementtrust-d1u1.vercel.app'],
 
     'allowed_origins' => ['*'],
 
