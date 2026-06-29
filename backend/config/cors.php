ab837cc30f59e5b58af75a917a8e95c04139565b
Almightymayor <danielmoses849@gmail.com>
2026-06-29 22:15:31 +0200

    Update logo and API routing

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,18 +6,11 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['https://elementtrust-d1u1.vercel.app'],
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
 
